491001e315f4fd5731c5b3100849683f7085ae53
TOMMY-ASTRO <waeop10@gmail.com>
2023-03-20 12:22:38 -0500

    Portafolio

---
+++ portafolio.js
@@ -0,0 +1,8 @@
+let typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Backend Developer", "Ethical Hacker"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true,
+
+})
